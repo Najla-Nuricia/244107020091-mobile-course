5b401ce458cf35508d7e352351e4fcca29049297
cecilyn <najlalaudy@gmail.com>
2026-09-10 18:25:40 +0700

    separate todo widget into todotile

--- 03-week-3-navigation-state-management/week3_todo/lib/providers/todo_page.dart
+++ 03-week-3-navigation-state-management/week3_todo/lib/providers/todo_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/todo_provider.dart';
+import '../providers/todo_tile.dart';
 
 class TodoPage extends ConsumerWidget {
   const TodoPage({super.key});
@@ -15,24 +16,15 @@ class TodoPage extends ConsumerWidget {
           ? const Center(child: Text('Belum ada tugas'))
           : ListView.builder(
               itemCount: todos.length,
-              itemBuilder: (context, index) => ListTile(
-                leading: Checkbox(
-                  value: todos[index].done,
-                  onChanged: (_) =>
-                      ref.read(todoListProvider.notifier).toggle(index),
-                ),
-                title: Text(
-                  todos[index].title,
-                  style: TextStyle(
-                      decoration: todos[index].done
-                          ? TextDecoration.lineThrough
-                          : null),
-                ),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () =>
-                      ref.read(todoListProvider.notifier).remove(index),
-                ),
+              itemBuilder: (context, index) 
+              =>TodoTile(
+                todo: todos[index],
+                onToggle: () {
+                  ref.read(todoListProvider.notifier).toggle(index);
+                },
+                onDelete: () {
+                  ref.read(todoListProvider.notifier).remove(index);
+                },
               ),
             ),
       floatingActionButton: FloatingActionButton(

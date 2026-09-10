c6f9cdd8919a4072a2b2ae303b68cccbb9fb824b
cecilyn <najlalaudy@gmail.com>
2026-09-10 20:15:09 +0700

    add navbar

---
+++ 03-week-3-navigation-state-management/week3_todo/lib/providers/navbar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Navbar extends StatelessWidget {
+  const Navbar({
+    super.key,
+    required this.child,
+    required this.location,
+  });
+
+  final Widget child;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedIndex = location == '/stats' ? 1 : 0;
+
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: selectedIndex,
+        onDestinationSelected: (index) {
+          if (index == 0) {
+            context.go('/');
+          } else {
+            context.go('/stats');
+          }
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.check_box_outlined),
+            label: 'ToDo',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bar_chart),
+            label: 'Statistik',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- 03-week-3-navigation-state-management/week3_todo/lib/router/app_router.dart
+++ 03-week-3-navigation-state-management/week3_todo/lib/router/app_router.dart
@@ -1,16 +1,28 @@
 import 'package:go_router/go_router.dart';
 import '../providers/todo_page.dart';
 import '../providers/stats_page.dart';
+import '../providers/navbar.dart';
 
 final router = GoRouter(
+  initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const TodoPage(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return Navbar(
+          location: state.uri.path,
+          child: child,
+        );
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const TodoPage(),
+        ),
+        GoRoute(
+          path: '/stats',
+          builder: (context, state) => const StatsPage(),
+        ),
+      ],
     ),
-    GoRoute(
-      path: '/stats',
-      builder: (context, state) => const StatsPage(),
-    )
   ],
 );
